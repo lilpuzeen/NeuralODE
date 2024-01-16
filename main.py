ce0b9df04d08ee706910516be27b2374c81322b7
armantovmasyan <arman-nn04@mail.ru>
2024-01-16 20:45:40 +0300

    feat: change comments language and pylint workflow modify

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from functions import *
 
-# Параметры экспериментов
+# Experimental parameters
 experiment_params = [
-(-2, 1, 3, -4), # Эксперимент 1
-(-1, 2, -3, 4), # Эксперимент 2
-(0.5, -1.5, 2, -2.5), # Эксперимент 3
-(1, -1, 1.5, -1.5), # Эксперимент 4
-(-2.5, 2.5, -0.5, 0.5), # Эксперимент 5
-(2, -2, 3, -3), # Эксперимент 6
-(-1.5, 1, 2.5, -2), # Эксперимент 7
-(1.5, -1, -2, 2), # Эксперимент 8
-(-3, 3, -1, 1), # Эксперимент 9
-(2.5, -2.5, 0.5, -0.5) # Эксперимент 10
+    (-2, 1, 3, -4),  # Experiment 1
+    (-1, 2, -3, 4),  # Experiment 2
+    (0.5, -1.5, 2, -2.5),  # Experiment 3
+    (1, -1, 1.5, -1.5),  # Experiment 4
+    (-2.5, 2.5, -0.5, 0.5),  # Experiment 5
+    (2, -2, 3, -3),  # Experiment 6
+    (-1.5, 1, 2.5, -2),  # Experiment 7
+    (1.5, -1, -2, 2),  # Experiment 8
+    (-3, 3, -1, 1),  # Experiment 9
+    (2.5, -2.5, 0.5, -0.5)  # Experiment 10
 ]
 
-# Запуск экспериментов
+# Running experiments
 for i, params in enumerate(experiment_params, start=1):
     conduct_experiment(*params, experiment_id=i)
